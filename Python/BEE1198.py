fa6940eb2ffe12e6d33a2bd8e1fde9fa26b21541
LuisCunha05 <luisf.cunha05@gmail.com>
2024-07-30 10:41:00 -0400

    BEE1198 Finalizado
    Adicionado exercício 1198

---
+++ Python/BEE1198.py
@@ -0,0 +1,13 @@
+#Hashmat e um guerreiro cujo seu grupo de soldados se move de um local a outro para lutar contra os seus oponentes. Antes da luta ele apenas calcula uma coisa. A diferença entre a quantidade de soldados
+# que possui e a quantidade de soldados oponentes. A partir desta diferença ele decide se vai ou não lutar. Às vezes Hashmat tem mais soldados do que o seu oponente, mas na maioria das vezes não.
+
+#Entrada
+#A entrada contém dois números inteiros em cada linha. Estes dois números denotam respectivamente a quantidade de soldados do exército de Hashmat e do seu oponente.  Nenhum número de entrada é maior do que  232.
+#A entrada termina com fim de arquivo (EOF).
+
+while(True):
+    try:
+        a , b = map(int, input().split(' '))
+        print(b - a if b - a >= 0 else a - b)
+    except EOFError:
+        break
