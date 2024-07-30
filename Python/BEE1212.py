fc2efa2cd94537160e1283a247800d849fd3f21c
LuisCunha05 <luisf.cunha05@gmail.com>
2024-07-30 08:12:24 -0400

    1212 finalizado

--- Python/BEE1212.py
+++ Python/BEE1212.py
@@ -2,10 +2,32 @@
 # na qual o valor 1 é carregado de uma posição para ser adicionado ao dígito seguinte) um desafio significativo. Seu trabalho é para contar o número de operações de carry
 # para cada um dos problemas de adição apresentados para que os educadores possam avaliar a sua dificuldade.
 
-a = b = ''
+a , b = map(int, input().split(' '))
 
-while(a != 0 and b != 0):
+while(not(a == 0 and b == 0)):
+    
+
+    carryCount = 0
+    lastA = 0
+    lastB = 0
+    carry = 0
+
+    while(not(a == 0 and b == 0)):
+        
+        lastA = a % 10
+        lastB = b % 10
+        a //= 10
+        b //= 10
+
+        if(lastA + lastB + carry >= 10):
+            carry = 1
+            carryCount += 1
+        else:
+            carry = 0
+    if(carryCount == 0):
+        print('No carry operation.')
+    elif(carryCount == 1):
+        print(f'1 carry operation.')
+    else:
+        print(f'{carryCount} carry operations.')
     a , b = map(int, input().split(' '))
-
-    print(a, b)
-
