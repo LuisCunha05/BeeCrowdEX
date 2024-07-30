5a8e23185d739f991449c4a5efd1bcd3b75ed415
LuisCunha05 <luisf.cunha05@gmail.com>
2024-07-29 23:06:26 -0300

    Adicionado Enunciado em 1212
    Foi adicionado enunciado no exerc 1212 como comentário

--- Python/BEE1212.py
+++ Python/BEE1212.py
@@ -1,3 +1,7 @@
+#As crianças são ensinadas a adicionar vários dígitos da direita para a esquerda, um dígito de cada vez. Muitos acham a operação "vai 1" (em inglês chamada de "carry",
+# na qual o valor 1 é carregado de uma posição para ser adicionado ao dígito seguinte) um desafio significativo. Seu trabalho é para contar o número de operações de carry
+# para cada um dos problemas de adição apresentados para que os educadores possam avaliar a sua dificuldade.
+
 a = b = ''
 
 while(a != 0 and b != 0):
